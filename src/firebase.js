accc6e61b327a8045920891da12c0769e7a761a4
zdrale <nikolazd22@outlook.com>
2023-06-10 14:56:52 +0200

    Adjusted folder structure, added firebase config

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyApHPOHDacjBLAHXWFqNnpTp76JNhdSWq8",
+  authDomain: "netflix-clone-8b9d4.firebaseapp.com",
+  projectId: "netflix-clone-8b9d4",
+  storageBucket: "netflix-clone-8b9d4.appspot.com",
+  messagingSenderId: "880427339318",
+  appId: "1:880427339318:web:15b6b24e7a239e90e71297",
+};
