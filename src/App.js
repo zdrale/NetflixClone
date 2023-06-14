e08ea3437841b6c4ee57c9effc5070a3cc68524e
zdrale <nikolazd22@outlook.com>
2023-06-14 12:36:49 +0200

    Made LoginScreen

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React from "react";
 import "./App.css";
 import HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginScreen from "./screens/LoginScreen";
 
 function App() {
+  const user = null;
   return (
     <div className="app">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomeScreen />} />
-        </Routes>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        )}
       </BrowserRouter>
     </div>
   );
