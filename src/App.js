352a61bc60abbda442f9d6323cb0c53dda26351a
zdrale <nikolazd22@outlook.com>
2023-06-15 13:22:14 +0200

    Added login functionality

--- src/App.js
+++ src/App.js
@@ -4,16 +4,26 @@ import HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginScreen from "./screens/LoginScreen";
 import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+
+  const dispatch = useDispatch();
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
-        //Logged iN
-        console.log(userAuth);
+        //Logged in
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
       } else {
         //Logged out
+        dispatch(logout);
       }
     });
 
